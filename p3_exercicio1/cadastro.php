71b5fc791d533eb719c80ed2d87d4670c1f49506
Jeremias Cassiano <cassiano726@gmail.com>
2016-01-08 20:21:48 -0200

    Correção do arquivo cadastro.php

--- p3_exercicio1/cadastro.php
+++ p3_exercicio1/cadastro.php
@@ -8,14 +8,14 @@ action="index.php?page=lista_clientes -->
 
                         <form  method="post" action="index.php?page=lista_clientes">
 
-                            <fieldset> <!-Sepeara as áreas ou formulários por grupos -->
+                            <fieldset> <!--Sepeara as áreas ou formulários por grupos -->
                                 <legend>
                                     Formulário de Cadastro.
                                 </legend>
                                 <label>ID:<input type="text" name="id" autofocus="#"></label>
                                 <label>Nome: <input type="text" name="nome" autofocus="#" ></label><br>
                                 <label>Email: <input type="email" name="email" autofocus="#"> </label><br>
-                           </fieldset>
+:                            </fieldset>
 
                             <input type="submit" name="enviar" value="enviar">
 
